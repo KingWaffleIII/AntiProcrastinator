8a88a0a0a2bd98c23f97688c8ccbf850b01a4752
KingWaffleIII <waffle@planetwaffle.net>
2024-04-10 20:49:37 +0100

    earrape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import datetime
 import json
+import multiprocessing
+import playsound
 import pyttsx3
 import random
 import time
@@ -36,12 +38,15 @@ async def say(text):
 
 
 def get_time(time: int):
-    if time >= 3600:  # More than or equal to an hour
+    if time > 3600:
         return f"{time // 3600} hour{'s' if time // 3600 > 1 else ''}, {time % 3600 // 60} minute{'s' if time % 3600 // 60 > 1 else ''} and {time % 3600 % 60} second{'s' if time % 3600 % 60 > 1 else ''}"
-    elif time >= 60:  # More than or equal to a minute
+    if time == 3600:
+        return f"{time // 60} hours{'s' if time // 60 > 1 else ''}"
+    if time > 60:
         return f"{time // 60} minute{'s' if time // 60 > 1 else ''} and {time % 60} second{'s' if time % 60 > 1 else ''}"
-    else:  # Just seconds
-        return f"{time} second{'s' if time > 1 else ''}"
+    if time == 60:
+        return f"{time // 60} minute{'s' if time // 60 > 1 else ''}"
+    return f"{time} second{'s' if time > 1 else ''}"
 
 
 def get_insult():
@@ -65,18 +70,52 @@ async def watcher():
                     f"There are only {(exam - datetime.datetime.today()).days} days left until your exams. " + get_insult()
                 )
 
+                annoying_proc = None
+                super_annoying_proc = None
                 start = time.time()
                 while True:
                     new_window = GetWindowText(GetForegroundWindow())
                     if window != new_window and new_window != '':
                         break
                     diff = round(time.time() - start)
-                    # every 5 mins
-                    if diff > 0 and diff % 300 == 0:
+                    # every minute
+                    if diff > 0 and diff % 60 == 0:
                         await say(
                             f"You've been procrastinating for {get_time(diff)} on {window}! " + get_insult()
                         )
+                        time.sleep(1)
+                        # 1 minute
+                        if diff == 60 and annoying_proc is None:
+                            # check if media is playing and pause it
+                            sessions = await MediaManager.request_async()
+                            current_session = sessions.get_current_session()
+                            if (
+                                    current_session
+                                    and current_session.get_playback_info().controls.is_pause_enabled
+                            ):
+                                await current_session.try_pause_async()
+                            annoying_proc = multiprocessing.Process(target=playsound.playsound, args=("annoying.mp3",))
+                            annoying_proc.start()
+                        # 2 minutes
+                        if diff == 120 and super_annoying_proc is None:
+                            # check if media is playing and pause it
+                            sessions = await MediaManager.request_async()
+                            current_session = sessions.get_current_session()
+                            if (
+                                    current_session
+                                    and current_session.get_playback_info().controls.is_pause_enabled
+                            ):
+                                await current_session.try_pause_async()
+                            if annoying_proc is not None:
+                                annoying_proc.kill()
+                            super_annoying_proc = multiprocessing.Process(target=playsound.playsound, args=("super_annoying.mp3",))
+                            super_annoying_proc.start()
                     time.sleep(1)
+
+                if annoying_proc is not None:
+                    annoying_proc.kill()
+                if super_annoying_proc is not None:
+                    super_annoying_proc.kill()
 
                 diff = round(time.time() - start)
                 if diff > 0:
