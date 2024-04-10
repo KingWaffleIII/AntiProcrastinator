28fa1fd72f48697efc7d5a8f76138488401a0239
KingWaffleIII <waffle@planetwaffle.net>
2024-04-10 19:00:00 +0100

    stuff

--- README.md
+++ README.md
@@ -1,30 +1,38 @@
 # AntiProcrastinator
 
-Your computer will yell at you for procrastinating.
+Your computer will yell at you for procrastinating. Yes, I was procrastinating when I made this.
 
 Features:
 
--   Immediately yells at you on launch
--   Monitors the focused window and yells at you if you're procrastinating (configurable blacklist)
-    -   Also pauses any music you're playing to yell at you
--   Regularly reminds you how close your exam(s) are (configurable)
--
+- Immediately yells at you on launch
+- Monitors the focused window and yells at you if you're procrastinating
+    - Matches blacklisted strings in the window name
+- Pauses any media you're playing to yell at you
+- Regularly reminds you how close your exam(s) are
+- Customisable insults
+- Realtime config changes (no need to restart the script)
+
 
 ## Usage:
 
--   Set the `exam_date` in `config.json`
--   Add any phrases you want to `blacklist` (e.g. discord, steam) for the focused window in `config.json`
+- Set the `exam_date` in `config.json`
+- Add any phrases you want to `blacklist` (e.g. discord, steam) for the focused window in `config.json`
+- Edit the `insults` in `config.json` to your liking
 
 Example:
 
 ```json
 {
-	"exam_date": "2024/05/09",
-	"blacklist": ["discord", "playnite", "steam"]
+    "exam_date": "2024/05/09",
+    "blacklist": ["discord", "steam"],
+    "insults": [
+    "Do you want to work at McDonalds?"
+    ]
 }
+
 ```
 
--   (recommended) Add to Task Scheduler
+- (recommended) Add to Task Scheduler
 
 This runs the script on startup and in the background.
 Enable this else TTS will not work:
--- main.py
+++ main.py
@@ -2,64 +2,91 @@ import asyncio
 import datetime
 import json
 import pyttsx3
+import random
 import time
-import win32api
 from win32gui import GetWindowText, GetForegroundWindow
 from winsdk.windows.media.control import (
     GlobalSystemMediaTransportControlsSessionManager as MediaManager,
 )
 
-
-data = json.load(open("config.json"))
-
-EXAM = datetime.datetime.strptime(data["exam_date"], "%Y/%m/%d")
+exam = datetime.datetime.strptime(json.load(open("config.json"))["exam_date"], "%Y/%m/%d")
 
 engine = pyttsx3.init()
+engine.setProperty("rate", 175)
 
 
-def startup():
-    engine.say(
-        f"There are only {(EXAM - datetime.datetime.today()).days} days left until your exams. Time to lock in!"
-    )
+async def say(text):
+    # check if media is playing and pause it
+    sessions = await MediaManager.request_async()
+    current_session = sessions.get_current_session()
+    paused = False
+    if (
+            current_session
+            and current_session.get_playback_info().controls.is_pause_enabled
+    ):
+        await current_session.try_pause_async()
+        paused = True
+
+    engine.say(text)
     engine.runAndWait()
 
+    # if we paused media, play it again
+    if paused:
+        await current_session.try_play_async()
 
-async def main():
+
+def get_time(time: int):
+    if time >= 3600:  # More than or equal to an hour
+        return f"{time // 3600} hour{'s' if time // 3600 > 1 else ''}, {time % 3600 // 60} minute{'s' if time % 3600 // 60 > 1 else ''} and {time % 3600 % 60} second{'s' if time % 3600 % 60 > 1 else ''}"
+    elif time >= 60:  # More than or equal to a minute
+        return f"{time // 60} minute{'s' if time // 60 > 1 else ''} and {time % 60} second{'s' if time % 60 > 1 else ''}"
+    else:  # Just seconds
+        return f"{time} second{'s' if time > 1 else ''}"
+
+
+def get_insult():
+    data = json.load(open("config.json"))
+    return data["insults"][random.randint(0, len(data["insults"]) - 1)]
+
+
+async def startup():
+    await say(
+        f"There are only {(exam - datetime.datetime.today()).days} days left until your exams. Time to lock in!"
+    )
+
+
+async def watcher():
     while True:
         try:
+            data = json.load(open("config.json"))
             window = GetWindowText(GetForegroundWindow())
             if any(x in window.lower() for x in data["blacklist"]):
-                # check if media is playing and pause it
-                sessions = await MediaManager.request_async()
-                current_session = sessions.get_current_session()
-                paused = False
-                if (
-                    current_session
-                    and current_session.get_playback_info().controls.is_pause_enabled
-                ):
-                    await current_session.try_pause_async()
-                    paused = True
-
-                engine.say(
-                    f"There are only {(EXAM - datetime.datetime.today()).days} days left until your exams. Stop procrastinating retard or ISIS will be your only entryway into aerospace!"
+                await say(
+                    f"There are only {(exam - datetime.datetime.today()).days} days left until your exams. " + get_insult()
                 )
-                engine.runAndWait()
-
-                # if we paused media, play it again
-                if current_session and paused:
-                    await current_session.try_play_async()
 
                 start = time.time()
                 while True:
-                    if window != GetWindowText(GetForegroundWindow()):
+                    new_window = GetWindowText(GetForegroundWindow())
+                    if window != new_window and new_window != '':
                         break
+                    diff = round(time.time() - start)
+                    # every 5 mins
+                    if diff > 0 and diff % 300 == 0:
+                        await say(
+                            f"You've been procrastinating for {get_time(diff)} on {window}! " + get_insult()
+                        )
                     time.sleep(1)
-                end = time.time()
 
-                text = f"You were procrastinating for {round(end - start)} seconds on {window}! "
-                print(text)
-                engine.say(text + "Absolute dumbass. Do you want to work at McDonalds?")
-                engine.runAndWait()
+                diff = round(time.time() - start)
+                if diff > 0:
+                    notice = (
+                        f"You were procrastinating for {get_time(diff)} on {window}! "
+                    )
+                    print(notice)
+                    await say(
+                        notice + get_insult()
+                    )
 
                 time.sleep(60)
 
@@ -69,5 +96,5 @@ async def main():
 
 
 if __name__ == "__main__":
-    startup()
-    asyncio.run(main())
+    asyncio.run(startup())
+    asyncio.run(watcher())
